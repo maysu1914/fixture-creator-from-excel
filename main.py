350d4cf33fb1a0e11b14e4b9456fc457d263a28f
maysu1914 <mustafa.aysu@hotmail.com>
2021-05-21 14:17:12 +0300

    rename some variables, rename browser class, remove redundant attributes of functions, update docs, optimize browser class initializing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import os
 import pathlib
 import time
 import urllib.parse
@@ -10,47 +9,46 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
 
 
-class BrowserChrome:
-    path = "\driver\chromedriver.exe"
-    extensions = ()
+class Browser:
 
-    def __init__(self, path=path, extensions=extensions, headless=None):
-        driver_path = os.getcwd() + path
-        print(driver_path)
-        options = self.get_options(extensions, headless)
-        self.driver = webdriver.Chrome(executable_path=driver_path, options=options)
+    def __init__(self, executable_path, extensions=[], headless=False):
+        options = chrome_options()
+        options.add_argument("--start-maximized")
+        options.headless = True
+        for extension in extensions:
+            options.add_extension(extension)
+        self.driver = webdriver.Chrome(executable_path=executable_path, options=options)
 
     def open(self, url):
         if urllib.parse.unquote(self.driver.current_url) != url:
             self.driver.get(url)
 
-    def get_driver(self):
-        return self.driver
-
-    def get_options(self, extensions, headless):
-        options = chrome_options()
-        options.add_argument("--start-maximized")
-        if headless:
-            options.headless = True  # disabled because some different behaviours detected from web site
-        for extension in extensions:
-            options.add_extension(os.getcwd() + extension)
-        return options
-
     def google_translate(self, text, _from, _to):
-        self.open(
-            f'https://translate.google.com.tr/?hl={_to}&tab=wT&sl={_from}&tl={_to}&text={urllib.parse.quote_plus(text)}&op=translate')
+        """
+        it uses dedicated google translate page
+        :param text:
+        :param _from:
+        :param _to:
+        :return:
+        """
         result_xpath = """//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]/span[1]/span/span/text()"""
+        try_again_button_xpath = """//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[4]/div[2]/button/span"""
+        translate_url = f'https://translate.google.com.tr/?hl={_to}&tab=wT&sl={_from}&tl={_to}&text={text}&op=translate'
+
+        self.open(translate_url)
+
         while not html.fromstring(self.driver.page_source).xpath(result_xpath):
-            try_again_button_xpath = """//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[4]/div[2]/button/span"""
             if self.driver.find_element_by_xpath(try_again_button_xpath).is_displayed():
-                print('try again exist')
+                print('Trying again...' + f"{text}: {_from} -> {_to}")
                 self.driver.find_element_by_xpath(try_again_button_xpath).click()
         return html.fromstring(self.driver.page_source).xpath(result_xpath)[0]
 
-    def google_search_translate(self, text):
+    def google_search_translate(self, text, timeout=1.5):
         """
-        better than individual translate page
+        it uses google search translate module
+        gives better results than dedicated translate page
         but not well programmed
+        :param timeout:
         :param text:
         :return:
         """
@@ -62,13 +60,14 @@ class BrowserChrome:
         self.driver.find_element_by_xpath(source).clear()
         self.driver.find_element_by_xpath(source).send_keys(text)
 
-        time.sleep(1.5)
-        print(' -> '.join([text, html.fromstring(self.driver.page_source).xpath(result)[0]]))
+        time.sleep(timeout)
+
+        print("Translated: " + ' -> '.join([text, html.fromstring(self.driver.page_source).xpath(result)[0]]))
         return html.fromstring(self.driver.page_source).xpath(result)[0]
 
 
 def read_excel(filepath, columns=None):
-    df = pd.read_excel(filepath, header=0, usecols=','.join(columns))
+    df = pd.read_excel(filepath, usecols=','.join(columns))
     headers = [header for header in df]
     data = {header: '' for header in headers}
     results = []
@@ -83,7 +82,7 @@ def read_excel(filepath, columns=None):
 
 
 def get_json_countries(data):
-    bc = BrowserChrome(headless=True)
+    browser = Browser(executable_path="driver/chromedriver.exe", headless=True)
     countries = {}
     # set country names only as keys
     counter = 1
@@ -92,7 +91,7 @@ def get_json_countries(data):
             countries[row['country']] = {
                 'pk': counter,  # we use this id when creating city fixture
                 'fields': {
-                    'formal_name': bc.google_search_translate(row['country']),
+                    'formal_name': browser.google_search_translate(row['country']),
                     'international_formal_name': row['country'],
                 }
             }
