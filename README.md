7987efdd7a7409e76e3cf60cede65748589b37f1
maysu1914 <mustafa.aysu@hotmail.com>
2022-05-28 18:13:29 +0300

    overall structural changes, remove google translate developments, improve readability

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# fixture-creator-from-excel-plus-google-translate
-You can convert your data in excel to django fixture file easily, and there are 2 different google translate selenium function too
+# fixture-creator-from-excel
+You can convert your data in excel to django fixture file easily
 
 - pip install -r requirements.txt
-- download the suitable selenium driver to your chrome browser:
-  - https://chromedriver.chromium.org/downloads
 - worldcities provided by simplemaps.com for free:
   - https://simplemaps.com/data/world-cities
   
--- main.py
+++ main.py
@@ -1,125 +1,76 @@
 import json
 import pathlib
-import time
-import urllib.parse
+from functools import lru_cache
 
 import pandas as pd
-from lxml import html
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as chrome_options
+import unidecode as unidecode
 
 
-class Browser:
+class CountryCityFixture:
 
-    def __init__(self, executable_path, extensions=[], headless=False):
-        options = chrome_options()
-        options.add_argument("--start-maximized")
-        options.headless = headless
-        for extension in extensions:
-            options.add_extension(extension)
-        self.driver = webdriver.Chrome(executable_path=executable_path, options=options)
+    def __init__(self, excel_name, country_app_name, country_model_name, city_app_name, city_model_name, **kwargs):
+        self.data = pd.read_excel(excel_name, keep_default_na=False, **kwargs)
+        self.country_app_name = country_app_name
+        self.country_model_name = country_model_name
+        self.city_app_name = city_app_name
+        self.city_model_name = city_model_name
 
-    def open(self, url):
-        if urllib.parse.unquote(self.driver.current_url) != url:
-            self.driver.get(url)
-
-    def google_translate(self, text, _from, _to):
-        """
-        it uses dedicated google translate page
-        :param text:
-        :param _from:
-        :param _to:
-        :return:
-        """
-        result_xpath = """//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]/span[1]/span/span/text()"""
-        try_again_button_xpath = """//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[4]/div[2]/button/span"""
-        translate_url = f'https://translate.google.com.tr/?hl={_to}&tab=wT&sl={_from}&tl={_to}&text={text}&op=translate'
-
-        self.open(translate_url)
-
-        while not html.fromstring(self.driver.page_source).xpath(result_xpath):
-            if self.driver.find_element_by_xpath(try_again_button_xpath).is_displayed():
-                print('Trying again...' + f"{text}: {_from} -> {_to}")
-                self.driver.find_element_by_xpath(try_again_button_xpath).click()
-        return html.fromstring(self.driver.page_source).xpath(result_xpath)[0]
-
-    def google_search_translate(self, text, timeout=1.5):
-        """
-        it uses google search translate module
-        gives better results than dedicated translate page
-        but not well programmed
-        :param timeout:
-        :param text:
-        :return:
-        """
-        self.open("https://www.google.com/search?q=çeviri")
-
-        source = """//*[@id="tw-source-text-ta"]"""
-        result = """//*[@id="tw-target-text"]/span/text()"""
-
-        self.driver.find_element_by_xpath(source).clear()
-        self.driver.find_element_by_xpath(source).send_keys(text)
-
-        time.sleep(timeout)
-
-        print("Translated: " + ' -> '.join([text, html.fromstring(self.driver.page_source).xpath(result)[0]]))
-        return html.fromstring(self.driver.page_source).xpath(result)[0]
-
-
-def read_excel(filepath, columns=None):
-    df = pd.read_excel(filepath, usecols=','.join(columns))
-    return df.to_dict()
-
-
-def get_json_countries(data):
-    browser = Browser(executable_path="driver/chromedriver.exe", headless=True)
-    countries = {}
-
-    # remove duplicates by value but preserve at least one key (this will be a value for new list)
-    # then sort the result by new keys
-    for index, row in enumerate(sorted({value: key for key, value in data['country'].items()}.items()), start=1):
-        row_index = row[1]
-        country = row[0]
-        entry = {
-            'pk': index,  # we will use this id when creating city fixture
-            'fields': {
-                'formal_name': browser.google_search_translate(country),
-                'international_formal_name': country,
-                'iso2': data['iso2'][row_index],
-                'iso3': data['iso3'][row_index],
+    @property
+    @lru_cache
+    def countries(self):
+        countries = list()
+        seen = set()
+        for key, row in self.data.sort_values(by=['country']).iterrows():
+            if not row['country'] or row['country'] in seen:
+                continue
+            entry = dict(model=None, pk=None, fields=dict)
+            entry['fields'] = {
+                'name': row['country'],
+                'iso2': row['iso2'],
+                'iso3': row['iso3'],
             }
-        }
-        countries[country] = entry
-    return countries
+            countries.append(entry)
+            seen.add(row['country'])
+        self.add_fixture_attributes(self.country_app_name, self.country_model_name, countries)
+        return countries
 
-
-def get_json_cities(data, countries_json):
-    cities = {}
-    # set city names only as keys
-    for row in sorted({str(value): key for key, value in data['admin_name'].items()}.items()):
-        row_index = row[1]
-        admin_name = row[0]
-        entry = {
-            'fields': {
-                'country': countries_json[data['country'][row_index]]['pk'],
-                'name': admin_name,
-                'capital': True if data['capital'][row_index] == "primary" else False,
+    @property
+    @lru_cache
+    def cities(self):
+        cities = list()
+        seen = set()
+        for key, row in self.data.sort_values(by=['country', 'admin_name', 'city_ascii']).iterrows():
+            name = unidecode.unidecode(row['admin_name'] or row['city_ascii']).replace('/', '')
+            country_id = self.get_country_id(row['country'])
+            if not name or (name, country_id) in seen:
+                continue
+            entry = dict(model=None, pk=None, fields=dict)
+            entry['fields'] = {
+                'country': country_id,
+                'name': name,
+                'is_capital': row['capital'] == "primary",
             }
-        }
-        cities[data['admin_name'][row_index]] = entry
-    return cities
+            cities.append(entry)
+            seen.add((name, country_id))
+        self.add_fixture_attributes(self.city_app_name, self.city_model_name, cities)
+        return cities
 
+    @staticmethod
+    def add_fixture_attributes(app, model, objects):
+        for index, obj in enumerate(objects, start=1):
+            record = {
+                "model": f"{app}.{model}",
+                "pk": index,
+            }
+            obj.update(record)
+        return objects
 
-def create_django_fixture(app, model, fields_list):
-    fixture_list = []
-    for index, fields in enumerate(fields_list, start=1):
-        record = {
-            "model": f"{app}.{model}",
-            "pk": index,
-            "fields": fields
-        }
-        fixture_list.append(record)
-    return fixture_list
+    @lru_cache
+    def get_country_id(self, name):
+        for country in self.countries:
+            if country['fields']['name'] == name:
+                return country['pk']
+        raise ValueError
 
 
 def create_file(output_name, data):
@@ -130,20 +81,9 @@ def create_file(output_name, data):
 
 
 def main():
-    world_cities = read_excel('worldcities.xlsx', columns=['A', 'B', 'E', 'F', 'G', 'H', 'I'])
-
-    countries_json = get_json_countries(world_cities)
-    cities_json = get_json_cities(world_cities, countries_json)
-
-    country_fields_only = [value['fields'] for key, value in countries_json.items()]
-    country_fixture = create_django_fixture('information', 'country', country_fields_only)
-
-    create_file('fixtures/country.json', json.dumps(country_fixture, ensure_ascii=False))
-
-    city_fields_only = [value['fields'] for key, value in cities_json.items()]
-    city_fixture = create_django_fixture('information', 'city', city_fields_only)
-
-    create_file('fixtures/city.json', json.dumps(city_fixture, ensure_ascii=False))
+    fixture = CountryCityFixture('worldcities.xlsx', 'adverts', 'country', 'adverts', 'city')
+    create_file('fixtures/country.json', json.dumps(fixture.countries, ensure_ascii=False))
+    create_file('fixtures/city.json', json.dumps(fixture.cities, ensure_ascii=False))
 
 
 if __name__ == '__main__':
